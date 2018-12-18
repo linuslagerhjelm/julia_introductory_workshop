94f28f44a778cf9fa99d64b9ea16443eb96544b8
Linus <linus.lagerhjelm@gmail.com>
2018-12-18 10:26:16 +0100

    Add Create event

--- Tickify/src/Tickify.jl
+++ Tickify/src/Tickify.jl
@@ -2,6 +2,20 @@
 module Tickify
 
 include("types.jl")
-export Visitor, Organizer, Ticket
+export Visitor, Organizer, Ticket, Event, Standard, create_event
+
+function create_event(adult::Bool, organizer::Organizer; royal = 0, premium = 0, standard = 0, economy = 0)::Event
+    seats = Vector{Seat}(undef, royal + premium + standard + economy)
+    number = 0
+    for i in 1:royal seats[number] = Seat(number += 1, Royal, false) end
+    for i in 1:premium seats[number] = Seat(number += 1, Premium, false) end
+    for i in 1:standard seats[number] = Seat(number += 1, Standard, false) end
+    for i in 1:economy seats[number] = Seat(number += 1, Economy, false) end
+    Event(adult, organizer.uuid, seats)
+end
+
+function buy_ticket(event::Event, segment::Segment, user::User)::Ticket
+    # The price is computed as "# of seats" / "# of available seats" * segment type * 100
+end
 
 end # module
--- Tickify/test/runtests.jl
+++ Tickify/test/runtests.jl
@@ -12,12 +12,21 @@ using Tickify
 
         @test unique_ids()
         @test Visitor("abc", UInt(10), Set{Ticket}()) == Visitor("abc", UInt(10), Set{Ticket}())
-        @test Organizer("abc", UInt(10), Set{Ticket}()) == Organizer("abc", UInt(10), Set{Ticket}())
+        @test Organizer("abc", "omegapoint", Set{Event}()) == Organizer("abc", "omegapoint", Set{Event}())
     end
 
     @testset "Create Event" begin
+        organizer = Organizer("omegapoint")
+        event = create_event(true, organizer, standard=100)
+        
+        @test length(event.seats) == 100
+        @test event.adult
+        @test event.seats[1].segment == Standard
+        @test event.organizer == organizer.uuid
     end
 
     @testset "Buy Tickets" begin
+        organizer = Organizer("omegapoint")
+        event = create_event(true, organizer, premium=50, standard=100)
     end
 end
