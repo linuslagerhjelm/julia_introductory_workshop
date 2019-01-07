cd2b93efb236d8897df0f9d4eca6e4a359973c78
Linus <linus.lagerhjelm@gmail.com>
2019-01-07 15:44:24 +0100

    Remove solutions from master branch

--- Tickify/src/Tickify.jl
+++ Tickify/src/Tickify.jl
@@ -17,32 +17,8 @@ export
 
 function create_event(name::String, adult::Bool, organizer::Organizer; royal = 0, premium = 0, standard = 0, economy = 0)::Event
     # Seats are distributed so that more expensive segments have lower numbers
-    seats = Vector{Seat}(undef, royal + premium + standard + economy)
-    number = 0
-    
-    for i in 1:royal seats[number] = Seat(number += 1, Royal, false) end
-    for i in 1:premium seats[number] = Seat(number += 1, Premium, false) end
-    for i in 1:standard seats[number] = Seat(number += 1, Standard, false) end
-    for i in 1:economy seats[number] = Seat(number += 1, Economy, false) end
-
-    Event(name, adult, organizer.uuid, seats)
 end
 
-function split_seats(seats::Vector{Seat})::Tuple{Vector{Seat}, Vector{Seat}}
-    free = Vector{Seat}()
-    taken = Vector{Seat}()
-    
-    for seat in seats
-        if seat.taken push!(taken, seat) else push!(free, seat) end
-    end
-    
-    (free, taken)
-end
-get_segment(seats::Vector{Seat}, segment::Segment) = filter(seat -> seat.segment == segment, seats)
-function get_best_segment(seats::Vector{Seat}, segment::Segment)
-    free = get_segment(seats, segment)
-    if length(free) > 0 free else seats end
-end
 function buy_ticket!(event::Event, segment::Segment, visitor::Visitor)::Optional{Ticket}
     # The price is computed as "# of seats" / "# of available seats" * (segment type) + 1 * 100
     # The seat is selected as the first available seat that matches the segment preference
